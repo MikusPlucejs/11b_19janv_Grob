82c4b94e3961c1d402b7a56572db0a436b35d9ad
Mikus <mikus.plucejs@grobinasskola.lv>
2024-01-19 09:57:10 +0200

    3uzd

---
+++ 3uzd.py
@@ -0,0 +1,16 @@
+# Izveidot Python programmu, kas nolasītu un izdrukātu trešās teksta faila rindas saturu.
+
+def r4(tkst1):
+
+    with open(tkst1, 'r', encoding='utf-8') as f34:
+            rinds = f34.readlines()
+
+            if len(rinds) >= 3:
+                rinda3 = rinds[2].strip()
+                print(f"Trešās rindas saturs: {rinda3}")
+            else:
+                print("Failā nav pietiekami daudz rindu.")
+
+
+tkst1 = '1uzd.txt'
+r4(tkst1)
